fbdc1ecc6c5c3d26ab5f5acd1fc9b52dc144dfcd
Avel7884 <Avel7884@users.noreply.github.com>
2019-07-03 16:27:25 +0500

    method switch

--- FocusScoring/Settings.cs
+++ FocusScoring/Settings.cs
@@ -3,5 +3,6 @@ namespace FocusScoring
     public static class Settings
     {
         public static string FocusKey { get; set; }
+        public static string OgrnEnabled { get; set; }
     }
 }
--- FocusScoring/XmlDownload.cs
+++ FocusScoring/XmlDownload.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Xml;
@@ -7,8 +10,8 @@ namespace FocusScoring
     internal class XmlDownload : IXmlAccess
     {
         private readonly string focusKey;
-        //private static XmlReader reader;
-
+        
+        
         public XmlDownload(string focusKey)
         {
             this.focusKey = focusKey;
@@ -16,9 +19,8 @@ namespace FocusScoring
         
         public bool TryGetXml(string inn, ApiMethod method, out XmlDocument document)
         {
-        //public XmlDocument GetXml(string sUrl)
             //TODO refactor
-            var wrGETURL = WebRequest.Create($"https://focus-api.kontur.ru/api3/{method}?key={focusKey}&inn={inn}&xml");
+            var wrGETURL = WebRequest.Create($"https://focus-api.kontur.ru/api3/{GetMethodName(method)}?key={focusKey}&inn={inn}&xml");
             Stream webStream = null;
             var netfail=false;
             try{webStream = wrGETURL.GetResponse().GetResponseStream();}
@@ -26,11 +28,22 @@ namespace FocusScoring
             document = new XmlDocument();
             try {using (var reader = XmlReader.Create(webStream)) { document.Load(reader);}}
             catch {netfail = true;}
-
-            //if (netfail)
-                //return null;//new[] {"Ошибка! Проверьте подключение к интернет и повторите попытку."};
             
             return !netfail;
+        }
+
+        private string GetMethodName(ApiMethod method)
+        {
+            switch (method)
+            {
+                case ApiMethod.analytics: return "analytics";
+                case ApiMethod.req: return "req";
+                case ApiMethod.buh: return "buh";
+                case ApiMethod.contacts: return "contacts";
+                case ApiMethod.licences: return "licences";
+                case ApiMethod.egrDetails: return "egrDetails";
+                default: throw new ArgumentException("Unknown method wtf");
+            }
         }
     }
 }
