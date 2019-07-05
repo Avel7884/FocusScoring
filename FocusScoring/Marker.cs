64ad2c1337ad2fb099aa91aed1d73a3377fdebae
Avel7884 <Avel7884@users.noreply.github.com>
2019-07-05 12:04:52 +0500

    Marker class

---
+++ FocusScoring/Marker.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FocusScoring
+{
+    public class Marker
+    {
+        
+        
+        public string Name { get; }
+        public string Desctiption { get; }
+
+        public bool Check()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
