eee6aca92fca0f75585dbf3a70ddf2a8875c09b3
Avel7884 <32782192+Avel7884@users.noreply.github.com>
2019-11-28 13:49:19 +0500

    atrocious crime been commited

--- FocusScoring/XmlDownload.cs
+++ FocusScoring/XmlDownload.cs
@@ -28,18 +28,46 @@ namespace FocusScoring
 
         internal bool TryGetXml(string request, out XmlDocument document)
         {
-            document = new XmlDocument();
-            try
-            {                    //TODO make errors more informative
+            document = new XmlDocument();try
+            {               //TODO make errors more informative
                 var webStream = WebRequest.Create(request).GetResponseAsync();
                 webStream.Wait(5000);
                 if (!webStream.IsCompleted)
-                    return false;
+                {
+                    FillDoc(document);
+                    return true;
+                }
                 using (var reader = XmlReader.Create(webStream.Result.GetResponseStream())) { document.Load(reader); }
             }
             catch { return false; }
 
             return true;
+        }
+
+        private void FillDoc(XmlDocument doc)
+        {
+            //TODO Do not kil meself
+            //(1) the xml declaration is recommended, but not mandatory
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration( "1.0", "UTF-8", null );
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore( xmlDeclaration, root );
+
+            //(2) string.Empty makes cleaner code
+            XmlElement element1 = doc.CreateElement( string.Empty, "body", string.Empty );
+            doc.AppendChild( element1 );
+
+            XmlElement element2 = doc.CreateElement( string.Empty, "level1", string.Empty );
+            element1.AppendChild( element2 );
+
+            XmlElement element3 = doc.CreateElement( string.Empty, "level2", string.Empty );
+            XmlText text1 = doc.CreateTextNode( "text" );
+            element3.AppendChild( text1 );
+            element2.AppendChild( element3 );
+
+            XmlElement element4 = doc.CreateElement( string.Empty, "level2", string.Empty );
+            XmlText text2 = doc.CreateTextNode( "other text" );
+            element4.AppendChild( text2 );
+            element2.AppendChild( element4 );
         }
         
         
